047e1c91d68ff0c55453934571751ff543931cfc
pearapple <stickytoffeepuddingwithcaramel@gmail.com>
2022-03-05 22:18:56 +0000

    Shortened Euler.hs by using Easy impl of Haskell-chart

--- app/Euler.hs
+++ app/Euler.hs
@@ -1,11 +1,7 @@
 module Euler where
 
-import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
-import Data.Colour(opaque)
-import Data.Colour.Names
-import Control.Lens
-import Data.Default.Class
 
 {-
 HOW TO USE:
@@ -20,34 +16,13 @@ tmin = 0.0 :: Float
 tmax = 10.0 :: Float
 y_0 = 1.0 :: Float
 f t = -t
-lineWidth = 2 :: Double
 
 ts = [tmin,tmin+dt..tmax] -- small error due to using ranges with floats
 ys f = take (length ts) $ iterate (\y_i -> y_i + f y_i * dt) y_0
 
-main = renderableToFile def "euler.png" $ toRenderable layout
-  where
-    -- euler method line
-    eulerproj = plot_lines_style . line_color .~ opaque blue
-      $ plot_lines_style . line_width .~ lineWidth
-      $ plot_lines_values .~ [[(x,y) | (x,y) <- zip ts $ ys f]]
-      $ def
-
-    -- actual func line
-    actual = plot_lines_style . line_color .~ opaque red
-      $ plot_lines_style . line_width .~ lineWidth
-      $ plot_lines_values .~ [[(x,y) | (x,y) <- zip ts $ map (\t -> exp(-t)) ts]]
-      $ def
-
-    -- ratio of euler to actual line
-    ratio = plot_lines_style . line_color .~ opaque purple
-      $ plot_lines_style . line_width .~ lineWidth
-      $ plot_lines_values .~ [[(x,y) | (x,y) <- zip ts $ zipWith (/) (ys f) $ map (\t -> exp(-t)) ts]]
-      $ def
-
-    -- general graph settings
-    layout = layout_title .~ "Euler Method"
-      $ layout_x_axis . laxis_override .~ axisGridHide
-      $ layout_plots .~ [toPlot actual, toPlot eulerproj, toPlot ratio]
-      $ layout_grid_last .~ False
-      $ def
+main = toFile def "euler.png" $ do
+  layout_title .= "Euler Method"
+  let actYs = [exp(-t) | t <- ts]
+  plot (line "Euler Projection" [[(x,y) | (x,y) <- zip ts $ ys f]])
+  plot (line "Actual Function" [[(x,y) | (x,y) <- zip ts actYs]])
+  plot (line "Ratio" [[(x,y) | (x,y) <- zip ts $ zipWith (/) (ys f) actYs]])
